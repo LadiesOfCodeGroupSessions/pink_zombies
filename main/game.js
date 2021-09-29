75e5e04f6e65994ce86478b852892e135f30613d
Sarah Usher <sarahnusher@gmail.com>
2021-09-29 19:17:21 +0100

    refactor: more efficient for loop

--- main/game.js
+++ main/game.js
@@ -24,31 +24,14 @@ class Game {
 	}
 
 	anyLivingSurvivors() {
-		var returnVal = false
-
-		this.survivors.forEach((survivor) => {
-			console.log("in anyLivingSurvivors: ", survivor.checkIsAlive());
-			if(survivor.checkIsAlive()){
-				returnVal = true
+		for (var i =0; i < this.survivors.length; i++) {
+			var survivor = this.survivors[i]
+			if (survivor.checkIsAlive() === true) {
+				return true;
 			}
-		});
+		}
 
-		return returnVal
-
-
-		// for (var i =0; i < this.survivors.length; i++) {
-		// 	var survivor = this.survivors[i]
-		// 	console.log("in anyLivingSurvivors: ", survivor.checkIsAlive());
-		// 	if (survivor.checkIsAlive() === true) {
-		// 		console.log("HERERERER")
-		// 		return true;
-		// 	}
-		// }
-		//
-		// return false;
-
-
-		// for (var i = 0; i < SomeArrayOfObject.length; i++)
+		return false;
 	}
 }
 
